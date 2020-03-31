1e74ca8914a42eb2d0b8ffc2fe9fc443da6feca1
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-31 21:54:39 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,14 @@
+class Patient
 
+  attr_accessor :name, :appointment, :doctor
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
