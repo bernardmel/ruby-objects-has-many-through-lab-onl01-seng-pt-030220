350b8c370b034274e159ae4709b416ccc7d5a960
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-31 22:12:39 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,8 +12,16 @@ class Patient
     @@all
   end
 
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+
   def doctors
     appointments.collect {|appointment| appointment.doctor}
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(self, date, doctor)
   end
 
 end
